ef82716b12a036638642ef9aa09099768f2a838f
Tangeni Shikomba <tangenishikomba@gmail.com>
2020-04-09 08:54:38 -0400

    final gui updated

--- app/src/main/java/com/example/sortingapp/AppInterface.java
+++ app/src/main/java/com/example/sortingapp/AppInterface.java
@@ -5,17 +5,22 @@ import android.graphics.Color;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-public class AppInterface extends RelativeLayout {
+public class AppInterface extends GridLayout {
 
     private final int SIZE = 10;
 
     TextView[] numbers;
+    TextView message;
 
     public AppInterface(Context context, int screenHeight) {
         super(context);
+
+        setRowCount(11);
+        setColumnCount(3);
 
         buildGui(context, screenHeight);
 
@@ -23,10 +28,10 @@ public class AppInterface extends RelativeLayout {
 
     public void buildGui(Context context, int screenHeight){
         numbers = new TextView[10];
-
         numbers = new TextView[SIZE];
+        int i = 0;
 
-        for (int i = 0; i < SIZE; i++)
+        for (i = 0; i < SIZE; i++)
         {
             numbers[i] = new TextView(context);
             numbers[i].setId(TextView.generateViewId());
@@ -34,15 +39,42 @@ public class AppInterface extends RelativeLayout {
             numbers[i].setTextColor(Color.BLACK);
             numbers[i].setTextSize(TypedValue.COMPLEX_UNIT_SP, 30);
             numbers[i].setGravity(Gravity.CENTER);
+            numbers[i].setTextSize(13);
 
-            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(0, 0);
-            if (i > 0) params.addRule(RelativeLayout.BELOW, numbers[i-1].getId());
+            GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+//            if (i > 0) params.addRule(RelativeLayout.BELOW, numbers[i-1].getId());
             params.width = LayoutParams.MATCH_PARENT;
-            params.height = screenHeight/10;
-            params.topMargin = 1;
+            params.height = screenHeight/15;
+            params.rowSpec = GridLayout.spec(i, 1);
+            params.columnSpec = GridLayout.spec(1, 1);
+//            params.topMargin = 1;
+            params.setMargins(10,5,10,5);
             numbers[i].setLayoutParams(params);
             addView(numbers[i]);
         }
+
+        message = new TextView(context);
+        message.setId(TextView.generateViewId());
+        message.setBackgroundColor(Color.GRAY);
+        message.setTextColor(Color.BLACK);
+        message.setTextSize(TypedValue.COMPLEX_UNIT_SP, 35);
+        message.setGravity(Gravity.CENTER);
+        message.setText("45 swaps left");
+
+        GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+//            if (i > 0) params.addRule(RelativeLayout.BELOW, numbers[i-1].getId());
+        params.width = LayoutParams.MATCH_PARENT;
+        params.height = screenHeight/10;
+        params.rowSpec = GridLayout.spec(i, 1);
+        params.columnSpec = GridLayout.spec(1, 1);
+        params.topMargin = 1;
+        params.setMargins(10,10,10,20);
+        message.setLayoutParams(params);
+        addView(message);
+
+
+
+
     }
 
     public void showCurrent(int[] current)
